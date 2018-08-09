16e77f7e6762f4cce5ec1bee821343842b003c81
Vinita <vinni.smile26@gmail.com>
2018-08-09 05:45:43 +0000

    separated test caese modulewise

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,22 +3,29 @@ const app = require('../index');
 // const sayHello = require('../index').sayHello;
 // const addNumbers = require('../index').addNumbers;
 
+//Results
+sayHelloResult= app.sayHello();
+addNumbersResult = app.addNumbers(4,3)
 describe('Index', function(){
-    it('app should return hello', function(){
-        assert.equal(app.sayHello(),'hello');
+    describe('sayHello()', function(){
+        it('app should return hello', function(){
+            assert.equal(sayHelloResult,'hello');
+        })
+    
+        it('app should return string output', function(){
+            assert.typeOf(sayHelloResult,'string');
+        }) 
     })
-
-    it('app should return string output', function(){
-        assert.typeOf(app.sayHello(),'string');
-    })
-
-    it('add numbers should be above 5', function(){
-        let result = app.addNumbers(4,2);
-        assert.isAbove(result,5);
-    })
-
-    it('add numbers should return number', function(){
-        let result = app.addNumbers(1,2);
-        assert.typeOf(result,'number');
+    
+    describe('addNumbers()',function(){
+        it('add numbers should be above 5', function(){
+            //let result = app.addNumbers(4,2);
+            assert.isAbove(addNumbersResult,5);
+        })
+    
+        it('add numbers should return number', function(){
+            //let result = app.addNumbers(1,2);
+            assert.typeOf(addNumbersResult,'number');
+        })
     })
 })
